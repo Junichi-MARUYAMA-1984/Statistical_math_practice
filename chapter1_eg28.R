226f759d967cb245b71c4117d483d9b3b14d1373
Junichi MARUYAMA <jmaruyama.mbc@tmd.ac.jp>
2020-12-24 02:17:02 +0900

    Chapter1 E.g.28

---
+++ chapter1_eg28.R
@@ -0,0 +1,85 @@
+# Clear environment
+rm(list = ls())
+
+# Setting Japanese font (for MacOSX)
+# par(family= "HiraKakuProN-W3")
+
+# Chapter1 e.g.28
+# データ生成
+N <- 100
+p <- 1 # 単回帰
+X <- matrix(rnorm(N * p), ncol = p); X <- cbind(rep(1, N), X)
+beta <- c(1, 1)
+epsilon <- rnorm(N)
+y <- X %*% beta + epsilon
+
+# 信頼区間または予測区間を返す関数f(x)を定義。
+U <- solve(t(X) %*% X) # t(X) %*% Xの逆行列
+beta_hat <- U %*% t(X) %*% y # 回帰係数
+RSS <- sum((y - X %*% beta_hat)^2) # 残差二乗和
+RSE <- sqrt(RSS / (N - p - 1)) # beta_hatの平均値の標準誤差
+alpha <- 0.05 # 有意水準
+f <- function(x, a) { # a = 0なら信頼区間。a = 1なら予測区間。
+  x <- cbind(1, x)
+  range <- qt(df = N - p - 1, 1 - alpha / 2) * RSE * sqrt(a + x %*% U %*% t(x))
+  return(list(lower = x %*% beta_hat - range,
+              upper = x %*% beta_hat + range))
+}
+
+x_seq <- seq(-10, 10, 0.1)
+
+# グラフで信頼区間を表示
+lower_seq <- NULL
+upper_seq <- NULL
+for (x in x_seq) {
+  ci_ul <- f(x, 0)
+  lower_seq <- c(lower_seq, ci_ul$lower)
+  upper_seq <- c(upper_seq, ci_ul$upper)
+}
+x_lim <- c(min(x_seq), max(x_seq))
+y_lim <- c(min(lower_seq), max(upper_seq))
+plot(x_seq, lower_seq, col = "blue", # 信頼区間下限を青線で描画
+     xlim = x_lim,
+     ylim = y_lim,
+     xlab = "x",
+     ylab = "y",
+     type = "l")
+par(new = TRUE)
+plot(x_seq, upper_seq, col = "red", # 信頼区間上限を赤線で描画
+     xlim = x_lim,
+     ylim = y_lim,
+     xlab = "",
+     ylab = "",
+     type = "l",
+     axes = FALSE)
+par(new = TRUE)
+
+# グラフで予測区間を表示
+lower_seq <- NULL
+upper_seq <- NULL
+for (x in x_seq) {
+  pi_ul <- f(x, 1)
+  lower_seq <- c(lower_seq, pi_ul$lower)
+  upper_seq <- c(upper_seq, pi_ul$upper)
+}
+x_lim <- c(min(x_seq), max(x_seq))
+y_lim <- c(min(lower_seq), max(upper_seq))
+plot(x_seq, lower_seq, col = "blue", # 予測区間下限を青破線で描画
+     xlim = x_lim,
+     ylim = y_lim,
+     xlab = "x",
+     ylab = "y",
+     type = "l",
+     lty = 4,
+     axes = FALSE)
+par(new = TRUE)
+plot(x_seq, upper_seq, col = "red", # 予測区間上限を赤破線で描画
+     xlim = x_lim,
+     ylim = y_lim,
+     xlab = "",
+     ylab = "",
+     type = "l",
+     lty = 4,
+     axes = FALSE)
+par(new = TRUE)
+abline(beta_hat[1], beta_hat[2])
